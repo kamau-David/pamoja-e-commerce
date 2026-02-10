6276abf79cbaa0d6a456b60c06c55304256d19ea
kamau-David <dayvieek@gmail.com>
2026-02-10 23:29:50 +0300

    Initial commit with proper .gitignore

---
+++ android/app/src/main/kotlin/com/example/pamoja_eccomerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pamoja_eccomerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:pamoja_eccomerce/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'pamoja eccomerce',
+      home: LoginScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          bodyMedium: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          bodySmall: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
